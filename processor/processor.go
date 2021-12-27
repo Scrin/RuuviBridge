d6cdddeeb858774bba9d205a17e7103a83f2813f
Scrin <Microx256@gmail.com>
2021-12-27 15:32:42 +0200

    Config improvements

--- processor/processor.go
+++ processor/processor.go
@@ -23,27 +23,41 @@ func Run(config config.Config) {
 	}
 
 	fmt.Println("Starting data sources...")
+	datasourcesStarted := false
 	if config.GatewayPolling != nil && (config.GatewayPolling.Enabled == nil || *config.GatewayPolling.Enabled) {
 		stop := data_sources.StartGatewayPolling(*config.GatewayPolling, measurements)
 		defer func() { stop <- true }()
+		datasourcesStarted = true
 	}
 	if config.MQTTListener != nil && (config.MQTTListener.Enabled == nil || *config.MQTTListener.Enabled) {
 		stop := data_sources.StartMQTTListener(*config.MQTTListener, measurements)
 		defer func() { stop <- true }()
+		datasourcesStarted = true
+	}
+	if !datasourcesStarted {
+		fmt.Println("No datasources configured! Please check the config.")
 	}
 
 	fmt.Println("Starting data sinks...")
+	datasinksStarted := false
 	if config.Debug {
 		sinks = append(sinks, data_sinks.Debug())
+		datasinksStarted = true
 	}
 	if config.InfluxDBPublisher != nil && (config.InfluxDBPublisher.Enabled == nil || *config.InfluxDBPublisher.Enabled) {
 		sinks = append(sinks, data_sinks.InfluxDB(*config.InfluxDBPublisher))
+		datasinksStarted = true
 	}
 	if config.Prometheus != nil && (config.Prometheus.Enabled == nil || *config.Prometheus.Enabled) {
 		sinks = append(sinks, data_sinks.Prometheus(*config.Prometheus))
+		datasinksStarted = true
 	}
 	if config.MQTTPublisher != nil && (config.MQTTPublisher.Enabled == nil || *config.MQTTPublisher.Enabled) {
 		sinks = append(sinks, data_sinks.MQTT(*config.MQTTPublisher))
+		datasinksStarted = true
+	}
+	if !datasinksStarted {
+		fmt.Println("No data consumers/sinks configured! Please check the config.")
 	}
 
 	fmt.Println("Starting processing...")
